9d3dd08f5662e4952656c70c70188e53728e298c
Sensei_1 <maxim@senseimarketing.com>
2023-03-31 13:11:53 -0400

    adding time start for the videos, removing old videos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
 export const videos = [
-    { id: 1, title: 'SpaceX Falcon 9 Launch', url: 'https://www.youtube.com/watch?v=wbSwFU6tY1c' },
-    { id: 2, title: 'NASA Space Shuttle Launch', url: 'https://www.youtube.com/watch?v=2A8_9yNF9s4' },
-    { id: 3, title: 'Blue Origin New Shepard Launch', url: 'https://www.youtube.com/watch?v=4SVlLQ2_1_M' },
-    { id: 4, title: 'Virgin Galactic SpaceShipTwo Launch', url: 'https://www.youtube.com/watch?v=QJQnDvNkxLA' },
-    { id: 5, title: 'ESA Ariane 5 Launch', url: 'https://www.youtube.com/watch?v=VzMDfkZw0MU' },
-    { id: 6, title: 'Russian Soyuz Launch', url: 'https://www.youtube.com/watch?v=VJrE5rWf1c4' },
-    { id: 7, title: 'SpaceX Falcon Heavy Launch', url: 'https://www.youtube.com/watch?v=wbSwFU6tY1c' },
-    { id: 8, title: 'NASA Saturn V Launch', url: 'https://www.youtube.com/watch?v=xC1ZzjvGiYY' },
-    { id: 9, title: 'Blue Origin New Glenn Launch', url: 'https://www.youtube.com/watch?v=uvSFIW4hnmM' },
-    { id: 10, title: 'Virgin Orbit LauncherOne Launch', url: 'https://www.youtube.com/watch?v=ohughI3L_n8' },
-    { id: 11, title: 'Rocket Lab Electron Launch', url: 'https://www.youtube.com/watch?v=2wWwIIzHfVA' },
+    { id: 1, title: 'SpaceX Falcon 9 Launch', url: 'hhttps://www.youtube.com/watch?v=wbSwFU6tY1c', startTime: 1314 },
+    { id: 2, title: 'Space Shuttle Launch Audio', url: 'https://www.youtube.com/watch?v=OnoNITE-CLc', startTime: 82 },
+    { id: 3, title: 'STS-129 HD Launch', url: 'https://www.youtube.com/watch?v=zsJpUCWfyPE', startTime: 82 }
+
   ];
   
 
@@ -25,7 +18,7 @@ export const videos = [
               <h2>{video.title}</h2>
               <div className="video-container">
                 <iframe
-                  src={`https://www.youtube.com/embed/${video.url.split('v=')[1]}`}
+                  src={`https://www.youtube.com/embed/${video.url.split('v=')[1]}?start=${video.startTime}`}
                   title={video.title}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen

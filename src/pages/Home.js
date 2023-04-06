76b136e4cb6fa216474b11ffb248a21364079798
Sensei_1 <maxim@senseimarketing.com>
2023-04-06 13:50:03 -0400

    3 more vids added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,10 @@ import React from 'react';
 export const videos = [
     { id: 1, title: 'SpaceX Falcon 9 Launch', url: 'hhttps://www.youtube.com/watch?v=wbSwFU6tY1c', startTime: 1314 },
     { id: 2, title: 'Space Shuttle Launch Audio', url: 'https://www.youtube.com/watch?v=OnoNITE-CLc', startTime: 82 },
-    { id: 3, title: 'STS-129 HD Launch', url: 'https://www.youtube.com/watch?v=zsJpUCWfyPE', startTime: 82 }
-
+    { id: 3, title: 'STS-129 HD Launch', url: 'https://www.youtube.com/watch?v=zsJpUCWfyPE', startTime: 82 },
+    { id: 4, title: 'Shuttle launch (Hubble 2010 - STS 125)', url: 'https://www.youtube.com/watch?v=uuYoYl5kyVE', startTime: 55 }, 
+    { id: 5, title: 'The Final Launch of Discovery', url: 'https://www.youtube.com/watch?v=pAGYsqfXkyA', startTime: 214 },
+    { id: 6, title: 'Shuttle Atlantis STS-132', url: 'https://www.youtube.com/watch?v=5KygwcZ545U', startTime: 561 } //https://youtu.be/5KygwcZ545U?t=561
   ];
   
 
